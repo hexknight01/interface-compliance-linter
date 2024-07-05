a21519dcabe2a321b635ae44313988a08f3f3c47
nampn2 <nampn2@vng.com.vn>
2024-07-05 11:34:36 +0700

    fix: create new func

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -5,13 +5,52 @@ import (
 	"go/token"
 	"go/types"
 
+	"github.com/golangci/plugin-module-register/register"
 	"golang.org/x/tools/go/analysis"
 )
 
-var Analyzer = &analysis.Analyzer{
-	Name: "enforceMethods",
-	Doc:  "Confirms that each struct implements Validator interface and has GetResourceMappings method",
-	Run:  run,
+func init() {
+	register.Plugin("example", New)
+}
+
+type MySettings struct {
+	One   string    `json:"one"`
+	Two   []Element `json:"two"`
+	Three Element   `json:"three"`
+}
+
+type Element struct {
+	Name string `json:"name"`
+}
+
+type PluginExample struct {
+	settings MySettings
+}
+
+func New(settings any) (register.LinterPlugin, error) {
+	// The configuration type will be map[string]any or []interface, it depends on your configuration.
+	// You can use https://github.com/go-viper/mapstructure to convert map to struct.
+
+	s, err := register.DecodeSettings[MySettings](settings)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PluginExample{settings: s}, nil
+}
+
+func (f *PluginExample) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{
+		{
+			Name: "enforceMethods",
+			Doc:  "Confirms that each struct implements Validator interface and has GetResourceMappings method",
+			Run:  run,
+		},
+	}, nil
+}
+
+func (f *PluginExample) GetLoadMode() string {
+	return register.LoadModeSyntax
 }
 
 type TokenInfo struct {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nhatnam1198/interface-compliance-linter
 
 go 1.21
 
-require golang.org/x/tools v0.22.0
+require (
+	github.com/golangci/plugin-module-register v0.1.1
+	golang.org/x/tools v0.22.0
+)
